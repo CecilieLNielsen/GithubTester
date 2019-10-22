957cd3283e54bc141fb6b8c37817b666fe671591
Cecilie <cecilienielsen@live.dk>
2019-10-22 18:13:34 +0200

    Updated main

--- src/githubtester/GithubTester.java
+++ src/githubtester/GithubTester.java
@@ -15,7 +15,7 @@ public class GithubTester {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Yay i'm on github!");
     }
-    
+
 }
