78519ee3928ce0dd0b330006195a78c1fe8e00c5
Cecilie <cecilienielsen@live.dk>
2019-10-22 18:20:01 +0200

    Updated main

--- src/githubtester/GithubTester.java
+++ src/githubtester/GithubTester.java
@@ -16,6 +16,7 @@ public class GithubTester {
      */
     public static void main(String[] args) {
         System.out.println("Yay i'm on github!");
+        System.out.println("Another line");
     }
 
 }
